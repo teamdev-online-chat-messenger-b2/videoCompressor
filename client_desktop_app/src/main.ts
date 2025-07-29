127454b5f99601a78354d43c3791d36bbb98b9e0
tanakas-2021 <shugotanaka2020@gmail.com>
2025-07-29 23:08:04 +0900

    init_desktop_app

---
+++ client_desktop_app/src/main.ts
@@ -0,0 +1,21 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true, // 開発中のみ
+    },
+  });
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
